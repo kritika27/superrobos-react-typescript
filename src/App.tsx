3f4f084b3ea09dab8037ddc5f0a4435b977735b5
kritika <kritikasri27@gmail.com>
2021-04-08 04:44:53 +0530

    Added main App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import CardList from "./components/CardList/CardList";
+import { Search } from "./components/Search/Search";
+import "./App.css";
+import { superobos } from "./robos";
 
-function App() {
+interface robots {
+  id: number;
+  name: string;
+  place: string;
+}
+const App = () => {
+  const [robos] = useState<robots[]>(superobos);
+  const [searchField, setSearchField] = useState<string>("");
+
+  const onSearchChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchField(event.target.value);
+  };
+
+  const filteredRobots = robos.filter((robos) => {
+    return robos.name.toLowerCase().includes(searchField.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Super Robos</h1>
+      <Search onSearchChange={onSearchChange} />
+      <CardList robos={filteredRobots} />
     </div>
   );
-}
-
+};
 export default App;
